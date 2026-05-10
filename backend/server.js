fc0c9fc059dec20a920e8312a8661b8612e2bdef
Arnav arya <arnavarya@Arnavs-MacBook-Air.local>
2026-05-11 01:17:47 +0530

    Fix build compatibility

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import sqlite3 from 'sqlite3';
 import { fileURLToPath } from 'url';
